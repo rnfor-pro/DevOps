b849111488a5fff0bdea29f7c4a7c028a1f9d4c1
rnfor-pro <nfor.rudolph1@gmail.com>
2024-04-26 10:53:22 -0400

    SonarQube port added

--- main.tf
+++ main.tf
@@ -41,6 +41,14 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow traffic on port 9000 for SonarQube "
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
