22cd5f7e5f9480b42ec3eaaa2ce8e357f61004f0
rnfor-pro <nfor.rudolph1@gmail.com>
2024-11-17 13:25:33 -0500

    jenkins and tomcat  version updated

--- jenkins-01/install_jenkins.sh
+++ jenkins-01/install_jenkins.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
-
+sudo apt update -y
+sudo apt upgrade -y
+# Add Jenkins repository key 
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+# Add Jenkins repository to sources list
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+# Update package lists
+sudo apt-get update -y
+# Install Jenkins
+sudo apt-get install -y jenkins 
+# Install fontconfig and OpenJDK 17
+sudo apt-get install -y fontconfig openjdk-17-jre
+# Enable Jenkins service
+sudo systemctl enable jenkins
+# Fetch public IP address using curl
+public_ip=$(curl -s https://api.ipify.org)
+# Print the fetched IP address along with port 8080
+echo "Your Application is running on: $public_ip:8080"
+echo "Please use the following initial password to unlock Jenkins:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "script is running successfully……!!😀" 
+sudo apt install maven -y 
 # Docker
-
 sudo apt-get update -y
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+sleep 7
+sudo hostnamectl set-hostname jenkins-svr
 
-# Trivy
-
-sudo apt-get install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update -y
-sudo apt-get install trivy -y       
-
-# Jenkins
-
-sudo apt update -y
-# sudo apt install fontconfig openjdk-17-jre -y
-sudo apt-get install openjdk-11-jdk
-java -version
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+# Troubleshooting
+# If your jenkins is not running run the commands below
+JAVA_VERSION=$($JAVA -version 2>&1 | sed -n ';s/.* version "\([0-9]*\)\.\([0-9]*\)\..*".*/\1\2/p;')
 sudo systemctl start jenkins
-sudo systemctl enable jenkins
+# sudo systemctl status jenkins
 
-
-
-# Rename hostname
+    
+# Do NOT RUN THE COMMANDS BELOW UNLESS YOU WANT TO UNINSTALL JAVA
 # to uninstall openjdk
 
 # To uninstall OpenJDK (if installed). First check which OpenJDK packages are installed.
@@ -50,6 +53,7 @@ sudo systemctl enable jenkins
 
 # sudo dpkg --list | grep -i jdk
 
-echo "------------changing hostname--------------"
-sudo hostnamectl set-hostname jenkins-svr
-bash
+
+
+
+    
--- jenkins-01/install_tomcat.sh
+++ jenkins-01/install_tomcat.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 #Author:Prof Elvis N
 #Company: Etech Co. llc
-sudo apt update
-sudo apt-get install openjdk-11-jdk
-sudo groupadd tomcat
-sudo useradd -g tomcat -d /opt/tomcat -s /bin/false tomcat
-sudo mkdir -p /opt/tomcat
-cd /opt
-sudo chown -R tomcat:tomcat tomcat
+sudo useradd -m -d /opt/tomcat -U -s /bin/false tomcat
+sudo apt update -y
+sudo apt install default-jdk -y
 cd /tmp
-sudo wget  https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.33/bin/apache-tomcat-10.1.33.tar.gz
-
-sudo tar -xvf /tmp/apache-tomcat-10.1.33.tar.gz -C /opt/tomcat
-sudo ln -s /opt/tomcat/apache-tomcat-10.1.33 /opt/tomcat/latest
-sudo chown -RH tomcat: /opt/tomcat/latest
-sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
-echo "Apache Tomcat installation completed successfully."
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
+sudo tar xzvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
+sudo chown -R tomcat:tomcat /opt/tomcat/
+sudo chmod -R u+x /opt/tomcat/bin
 sudo hostnamectl set-hostname tomcatsrv
-bash
